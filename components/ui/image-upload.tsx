ff6b8e6922cf0619e93ae770981b4f26dc4da2b7
Pavlenco05 <pasapavl.09102030@gmail.com>
2024-10-02 01:17:59 +0300

    finla push changes

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -67,7 +67,7 @@ const ImageUpload: React.FC<ImageUploadProps> =({
                             onClick={onClick}
                         >
                             <ImagePlus className="h-4 w-4 mr-2" />
-                            Upload an Image
+                            Upload a Image
                         </Button>
                     )
                 }}
